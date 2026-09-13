64479d27643f27a742387932dd11a562ef1737a9
emanuelpyo <derylpyo@gmail.com>
2026-09-13 13:45:42 +0700

    fix @media and #menu CSS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,15 @@
 const header = document.querySelector("header");
+const menu = document.querySelector("#menu");
+const navLinks = document.querySelector(".nav-links");
 
 if (header) {
     window.addEventListener("scroll", () => {
         header.classList.toggle("scrolled", window.scrollY > 20);
+    });
+}
+
+if (menu && navLinks) {
+    menu.addEventListener("click", () => {
+        navLinks.classList.toggle("active");
     });
 }
